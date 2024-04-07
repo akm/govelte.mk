5c484941c152c8bcd2f05c0f5d9acf5a59ea53ff
akm <akm2000@gmail.com>
2024-04-07 11:42:27 +0900

    :sparkles: Add src/index.ts with debug code

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { cwd, argv } from "node:process";
+import { resolve } from "node:path";
+
+// argv[0] is the path to the node executable
+// argv[1] is the path to the script being executed
+// argv[2] is the first argument after the script name
+// argv[3] is the second argument after the script name, and so on
+
+switch (argv[2]) {
+  case "dir":
+    console.log(resolve(__dirname, ".."));
+    break;
+  case "debug":
+    console.log("__dirname", __dirname);
+    console.log("process.cwd()", cwd());
+    console.log("argv", argv);
+    break;
+  default:
+    console.log("Unknown command");
+}
