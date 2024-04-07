11f79841d7a3d6aa00ba83760edd30cde696c000
akm <akm2000@gmail.com>
2024-04-07 22:23:37 +0900

    :sparkles: Add buf setup to makefiles/apisvr

--- makefiles/apisvr/setup.mk
+++ makefiles/apisvr/setup.mk
@@ -1,12 +1,17 @@
-go.mod:
-	go mod init apisvr
-
 .PHONY: setup
 setup: setup_connectrpc
+
+go.mod:
+	go mod init apisvr
 
 .PHONY: setup_connectrpc
 setup_connectrpc: go.mod
 	go install github.com/bufbuild/buf/cmd/buf@latest && \
 	go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest && \
 	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest && \
-	go install connectrpc.com/connect/cmd/protoc-gen-connect-go@latest
+	go install connectrpc.com/connect/cmd/protoc-gen-connect-go@latest && \
+	$(MAKE) asdf_reshim && \
+	$(MAKE) buf.yaml
+
+buf.yaml:
+	buf mod init
