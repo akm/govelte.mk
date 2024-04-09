b2d271b8c66d8f7f51a350b1db5d07a316e452aa
akm <akm2000@gmail.com>
2024-04-09 09:20:35 +0900

    :bug: Use SHELL to call npx buf generate with wildcards

--- makefiles/uisvr/connect-web.mk
+++ makefiles/uisvr/connect-web.mk
@@ -1,3 +1,5 @@
+
 .PHONY: generate
 generate:
-	npx buf generate ../../backends/apisvr/proto/**/*.proto
+	$$SHELL -c 'npx buf generate ../../backends/apisvr/proto/**/*.proto'
+# make can't treat asterisk as a wildcard, so we need to use shell to expand it
