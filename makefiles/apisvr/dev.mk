a3d3876c367eeb249d327fc1637bfb5ecb0c8889
akm <akm2000@gmail.com>
2024-05-06 21:33:23 +0900

    :sparkles: Specify DEV_TARGET for dev.mk for apisvr and uisvr

--- makefiles/apisvr/dev.mk
+++ makefiles/apisvr/dev.mk
@@ -2,13 +2,13 @@
 dev: local_dev_up run
 
 .PHONY: run
-run:
+run: setup
 	$(DEV_ENVS) go run $(PATH_TO_CMD_APISVR)
 
 .PHONY: local_dev_up
 local_dev_up:
-	$(MAKE) -C $(PATH_TO_LOCALDEV) up
+	$(MAKE) -C $(PATH_TO_LOCALDEV) up DEV_TARGET=apisvr
 
 .PHONY: local_dev_down
 local_dev_down:
-	$(MAKE) -C $(PATH_TO_LOCALDEV) down
+	$(MAKE) -C $(PATH_TO_LOCALDEV) down DEV_TARGET=apisvr
--- makefiles/uisvr/dev.mk
+++ makefiles/uisvr/dev.mk
@@ -1,13 +1,15 @@
 # DEV_DEPS の定義は この .mk ファイルを include する前に定義してください
 .PHONY: dev
-dev:
-	$(DEV_ENVS) npm run dev
+dev: local_dev_up run
 
-.PHONY: backends_dev
-backends_dev:
-	$(DEV_ENVS) \
-		$(MAKE) -C $(PATH_TO_BACKENDS) dev
-
-.PHONY: dev_run
-dev_run:$(DEV_DEPS)
+.PHONY: run
+run: setup
 	$(DEV_ENVS) npx vite dev --port $(APP_PORT_UISVR_dev)
+
+.PHONY: local_dev_up
+local_dev_up:
+	$(MAKE) -C $(PATH_TO_LOCALDEV) up DEV_TARGET=uisvr
+
+.PHONY: local_dev_down
+local_dev_down:
+	$(MAKE) -C $(PATH_TO_LOCALDEV) down DEV_TARGET=uisvr
