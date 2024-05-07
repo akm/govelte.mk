9b042c3a0fea40fcce9ecc4e510165fbdf8cc7b0
akm <akm2000@gmail.com>
2024-05-08 08:45:48 +0900

    :shower: Remove description about swaggerui

--- makefiles/default/ports.mk
+++ makefiles/default/ports.mk
@@ -1,13 +1,13 @@
 # ## APP_PORT
 #
-# APP_ENV                        | APP_STAGE_TYPE           | rproxy | apisvr HTTP | apisvr gRPC | ui HTTP  | mysql    | firebase authentication | swagger ui
-# -------------------------------|--------------------------|--------|-------------|-------------|----------|----------|-------------------------|------------
-# server                         | staging,production       | 10000  | 8000        | 8080        | 4173     | 3306     | ?                       |
-# dev                            | local                    | 10000  | 8000        | 8080        | 5173     | 3306     | 9099                    | 8090
-# clients/uisvr/test:integration | staging,production       | 10000  | 8000        | 8080        | 4173     | 3306     | ?                       |
-# clients/uisvr/test:integration | local,github             | 10001  | 8001        | 8081        | 4173     | 3307     | 9090                    |
-# clients/uisvr/test:unit        | local,github             | -      | -           | -           | -        | -        | -                       |
-# servers/apisvr/test            | local,github             | -      | -           | -           | -        | 3311     | 9091                    |
+# APP_ENV                        | APP_STAGE_TYPE           | rproxy | apisvr HTTP | apisvr gRPC | ui HTTP  | mysql    | firebase authentication
+# -------------------------------|--------------------------|--------|-------------|-------------|----------|----------|-------------------------
+# server                         | staging,production       | 10000  | 8000        | 8080        | 4173     | 3306     | ?
+# dev                            | local                    | 10000  | 8000        | 8080        | 5173     | 3306     | 9099
+# clients/uisvr/test:integration | staging,production       | 10000  | 8000        | 8080        | 4173     | 3306     | ?
+# clients/uisvr/test:integration | local,github             | 10001  | 8001        | 8081        | 4173     | 3307     | 9090
+# clients/uisvr/test:unit        | local,github             | -      | -           | -           | -        | -        | -
+# servers/apisvr/test            | local,github             | -      | -           | -           | -        | 3311     | 9091
 
 APP_PORT_RPROXY_dev=10000
 APP_PORT_RPROXY_e2e_test=10001
@@ -22,4 +22,3 @@ APP_PORT_FIREBASE_AUTH_dev?=9099
 APP_PORT_FIREBASE_AUTH_e2e_test?=9090
 APP_PORT_FIREBASE_AUTH_unit_test?=9091
 APP_PORT_FIREBASE_EMULATOR_SUITE_dev?=4000
-APP_PORT_SWAGGERUI_dev?=8090
