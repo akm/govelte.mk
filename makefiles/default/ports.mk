189c830b3b0c6daa2542f857da6c0c2cdeb5a0d7
akm <akm2000@gmail.com>
2024-05-08 08:55:06 +0900

    :sparkles: Use 8000 and 8001 for rproxy

--- makefiles/default/ports.mk
+++ makefiles/default/ports.mk
@@ -2,15 +2,15 @@
 #
 # APP_ENV                        | APP_STAGE_TYPE           | rproxy | apisvr | ui   | mysql | firebase authentication
 # -------------------------------|--------------------------|--------|--------|------|-------|-------------------------
-# server                         | staging,production       | 10000  | 8080   | 4173 | 3306  | ?
-# dev                            | local                    | 10000  | 8080   | 5173 | 3306  | 9099
-# clients/uisvr/test:integration | staging,production       | 10000  | 8080   | 4173 | 3306  | ?
-# clients/uisvr/test:integration | local,github             | 10001  | 8081   | 4173 | 3307  | 9090
+# server                         | staging,production       | 8000   | 8080   | 4173 | 3306  | ?
+# dev                            | local                    | 8000   | 8080   | 5173 | 3306  | 9099
+# clients/uisvr/test:integration | staging,production       | 8000   | 8080   | 4173 | 3306  | ?
+# clients/uisvr/test:integration | local,github             | 8001   | 8081   | 4173 | 3307  | 9090
 # clients/uisvr/test:unit        | local,github             | -      | -      | -    | -     | -
 # servers/apisvr/test            | local,github             | -      | -      | -    | 3311  | 9091
 
-APP_PORT_RPROXY_dev=10000
-APP_PORT_RPROXY_e2e_test=10001
+APP_PORT_RPROXY_dev=8000
+APP_PORT_RPROXY_e2e_test=8001
 APP_PORT_APISVR_dev?=8080
 APP_PORT_APISVR_e2e_test?=8001
 APP_PORT_UISVR_dev?=5173
